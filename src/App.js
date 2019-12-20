9686014d6ee613208e8b34441516462484dd7e75
pkuphy <xiaowangzi61@gmail.com>
2019-12-21 00:30:51 +0800

    使用 dayjs 设置时间及显示相对时间

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import { Button, Input, Row } from 'antd';
+import { Button, Input, Row, Tooltip } from 'antd';
 import 'antd/dist/antd.css';
+
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import 'dayjs/locale/zh-cn';
+
 import './App.css';
 import tx01 from './tx01.svg';
 import tx02 from './tx02.svg';
 import tx03 from './tx03.svg';
 import tx04 from './tx04.svg';
 
+dayjs.extend(LocalizedFormat);
+dayjs.extend(relativeTime);
+dayjs.locale('zh-cn');
 
 const { TextArea } = Input;
 
-class App extends React.Component {
 
+class App extends React.Component {
   state = {
     tweet: "",
     tweets: [{
       avatar: tx02,
       username: "吃吃",
       tweet: "今天晴天，火锅串串钵钵鸡，奈雪喜茶一点点，下雨了打火锅，下雪了吃羊肉，冬至吃饺子，过年吃腊肉。",
-      time: "8分钟前"
+      timestamp: "1576858365"
     }, {
       avatar: tx03,
       username: "邪见冥加",
       tweet: "邪见，即邪恶之见，或者指错误的观点。喜剧角色身份，常会有些出乎人意料的言语和行为。",
-      time: "13分钟前"
+      timestamp: "1576159736"
     }, {
       avatar: tx04,
       username: "日暮山山山",
       tweet: "墨点无多泪点多，山河仍是旧山河。横流乱世杈椰树，留得文林细揣摹。",
-      time: "20分钟前"
-    }]
+      timestamp: "1566858365"
+    }],
   }
 
   onChange = ({ target: { value } }) => {
@@ -41,7 +50,7 @@ class App extends React.Component {
       avatar: tx01,
       username: "User",
       tweet: this.state.tweet,
-      time: "1分钟前"
+      timestamp: dayjs().unix(),
     }
     this.setState({
       tweet: "",
@@ -52,6 +61,7 @@ class App extends React.Component {
   render() {
     const { tweet, tweets } = this.state;
     const tweetLength = tweet.length;
+
     return (
       <div style={{ width: 800, margin: '100px auto' }}>
         <div style={{ margin: '24px 0' }}/>
@@ -72,12 +82,14 @@ class App extends React.Component {
 
         {
           tweets.map(tweet => (
-            <div className="App-l" key={tweet.time}>
+            <div className="App-l" key={tweet.timestamp}>
               <img src={tweet.avatar} className="App-t" alt="头像"/>
               <div className="App-k">
                 <h3>{tweet.username}</h3>
                 <p>{tweet.tweet}</p>
-                <span>{tweet.time}</span>
+                <Tooltip placement="right" title={dayjs(tweet.timestamp*1000).format('YYYY-MM-DD HH:mm:ss')}>
+                  <span>{dayjs().to(tweet.timestamp*1000)}</span>
+                </Tooltip>
               </div>
             </div>
           ))
