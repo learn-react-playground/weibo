d2afbd4ad837e1fdeaa29b1413b3ae61b9a7ea60
pkuphy <xiaowangzi61@gmail.com>
2019-12-20 18:36:24 +0800

    格式化代码

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-
 if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
